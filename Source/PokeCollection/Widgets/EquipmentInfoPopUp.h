0ab6acd1be39c89ef4752b1052f40e7df8143533
hoon <cjsstk805@gmail.com>
2020-03-18 00:36:53 +0900

    장비 정보창 팝업 추가

--- Source/PokeCollection/Widgets/EquipmentInfoPopUp.h
+++ Source/PokeCollection/Widgets/EquipmentInfoPopUp.h
@@ -14,4 +14,19 @@ class POKECOLLECTION_API UEquipmentInfoPopUp : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BackgroundButton = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* EquipmentImage = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* EquipmentName = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* EquipmentDesc = nullptr;
+
 };
