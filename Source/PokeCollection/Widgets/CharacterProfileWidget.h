a6a78d3431c593706ed7e35da5c71bb50584bf34
Origin <40546213+Origin516@users.noreply.github.com>
2020-05-25 19:34:32 +0900

    클래스 및 BP 밑작업
    메인캐릭터용 클래스 밑 BP 밑작업

---
+++ Source/PokeCollection/Widgets/CharacterProfileWidget.h
@@ -0,0 +1,17 @@
+// Copyright Me. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/CharacterBoxWidget.h"
+#include "CharacterProfileWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKECOLLECTION_API UCharacterProfileWidget : public UCharacterSlot
+{
+	GENERATED_BODY()
+	
+};
