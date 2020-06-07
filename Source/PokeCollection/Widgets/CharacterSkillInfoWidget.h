0be13a3f3b0be55993fb3635af97ee866b6ab839
hoon <cjsstk805@gmail.com>
2020-06-08 00:58:36 +0900

    스킬 슬롯 위젯 추가

--- Source/PokeCollection/Widgets/CharacterSkillInfoWidget.h
+++ Source/PokeCollection/Widgets/CharacterSkillInfoWidget.h
@@ -6,8 +6,30 @@
 #include "Widgets/InGameCharacterInfoWidget.h"
 #include "CharacterSkillInfoWidget.generated.h"
 
+UCLASS()
+class POKECOLLECTION_API USkillInfoSlot : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	//virtual void NativeConstruct() override;
+	//virtual void OnOpen() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UImage* SkillIcon = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SkillDamageText  = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SkillDescText = nullptr;
+
+
+};
+
 /**
- * 
+ * 캐릭터 스킬 정보가 나오는 위젯
  */
 UCLASS()
 class POKECOLLECTION_API UCharacterSkillInfoWidget : public UCharacterInfoContentWidget
@@ -17,4 +39,13 @@ class POKECOLLECTION_API UCharacterSkillInfoWidget : public UCharacterInfoConten
 public:
 	//virtual void NativeConstruct() override;
 	//virtual void OnOpen() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UVerticalBox* SkillSlotBox = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<USkillInfoSlot> SkillInfoSlotClass = nullptr;
+
+
 };
