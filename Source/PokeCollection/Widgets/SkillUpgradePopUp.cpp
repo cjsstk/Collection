98212757c605e1fa11bf3a45b8ea3482c803df48
hoon <cjsstk805@gmail.com>
2020-06-10 23:59:04 +0900

    스킬 업그레이드 팝업 추가

---
+++ Source/PokeCollection/Widgets/SkillUpgradePopUp.cpp
@@ -0,0 +1,17 @@
+// Copyright Me. All Rights Reserved.
+
+
+#include "SkillUpgradePopUp.h"
+
+#include "CMS.h"
+#include "PokeCharacter.h"
+#include "PokeCollectionCharacter.h"
+#include "PokeCollectionHUD.h"
+#include "PokeItem.h"
+#include "Widgets/InGameCharacterInfoWidget.h"
+#include "Widgets/EquipmentUpgradePopUp.h"
+
+#include "Image.h"
+#include "TextBlock.h"
+#include "Button.h"
+#include "ScrollBox.h"
---
+++ Source/PokeCollection/Widgets/SkillUpgradePopUp.h
@@ -0,0 +1,61 @@
+// Copyright Me. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SkillUpgradePopUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKECOLLECTION_API USkillUpgradePopUp : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	void InitInfo(int32 InEquipmentID);
+
+	UFUNCTION()
+	void OnBackgroundClicked();
+
+	UFUNCTION()
+	void OnUpgradeButtonClicked();
+
+private:
+	void SetNeedMoneyText(int32 InNeedMoney);
+	void TickCheckUpgradeEnable();
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BackgroundButton = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* SkillImage = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SkillName = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UScrollBox* NeedItemBox = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* NeedMoneyText = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* UpgradeButton = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UNeedItemSlot> NeedItemSlotClass;
+
+	UPROPERTY(Transient)
+	TArray<class UNeedItemSlot*> NeedItemSlots;
+
+	int32 NeedMoney = 0;
+	int32 SkillIndex = -1;
+
+	bool bUpgradeEnable = false;
+};
